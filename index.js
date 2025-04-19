5e3f5dc3a2f402a0d8dc82ce38028cd108584e30
Sai Shashank <104642011+Dharma-Raghava-Sai-Shashank@users.noreply.github.com>
2025-04-19 20:53:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ app.use(cookieParser());
 app.use(
   cors({
     origin: process.env.Frontend_URL,
+    credentials: false,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
